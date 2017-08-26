b65d9c0f86dcbb15b1f4bafaeaf5888a52d21899
Kishore Bhatia <bhatia.kishore@gmail.com>
2017-08-26 14:00:38 -0700

    Update escrow.sol

--- escrow.sol
+++ escrow.sol
@@ -1,27 +1,45 @@
 pragma solidity ^0.4.0;
 
-//Create file Ecrow.sol and create 3 variables: a buyer, a seller, and an arbiter
+//Create file Ecrow.sol and create 3 variables: a buyer, a seller, and a resolver
 contract Escrow {
-
+    // email edit function....
+    
   address public buyer;
   address public seller;
-  address public arbiter;
+  address public resolver;
   
-  function Escrow(address _seller, address _arbiter) {
-    buyer = msg.sender;
-    seller = _seller;
-    arbiter = _arbiter;
+ // mapping(address => uint) public balances;
+ // mapping(address => string) public profile;
+  
+   struct contractDetails{
+        address seller;
+        address buyer;
+        address resolver;
+        uint price;
+        string email;
+        string title;
+        string productURL;
+        string productCondition;
+    }
+  contractDetails cD;
+  function Escrow (address _seller, address _buyer, address _resolver, uint _price,  string _email,string _title, string _productURL, string _productCondition) payable 
+  {
+  cD = contractDetails (_seller, _buyer, _resolver, _price, _email, _title, _productURL, _productCondition);
   }
   
+  function deposit() payable returns (bool) {
+       return true;
+  }
+     
   function payoutToSeller() {
-    if(msg.sender == buyer || msg.sender == arbiter) {
-      seller.send(this.balance);
+    if(msg.sender == buyer || msg.sender == resolver) {
+      seller.transfer(this.balance);
     }
   }
   
   function refundToBuyer() {
-    if(msg.sender == seller || msg.sender == arbiter) {
-      buyer.send(this.balance);
+    if(msg.sender == seller || msg.sender == resolver) {
+      buyer.transfer(this.balance);
     }
   }
   
