f8370ddb286f4bcdb7db4562b1b1a19a95f8797d
Kishore Bhatia <bhatia.kishore@gmail.com>
2017-08-25 20:33:37 -0700

    Create escrow.sol

---
+++ escrow.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.4.0;
+
+//Create file Ecrow.sol and create 3 variables: a buyer, a seller, and an arbiter
+contract Escrow {
+
+  address public buyer;
+  address public seller;
+  address public arbiter;
+  
+  function Escrow(address _seller, address _arbiter) {
+    buyer = msg.sender;
+    seller = _seller;
+    arbiter = _arbiter;
+  }
+  
+  function payoutToSeller() {
+    if(msg.sender == buyer || msg.sender == arbiter) {
+      seller.send(this.balance);
+    }
+  }
+  
+  function refundToBuyer() {
+    if(msg.sender == seller || msg.sender == arbiter) {
+      buyer.send(this.balance);
+    }
+  }
+  
+  function getBalance() constant returns (uint) {
+    return this.balance;
+  }
+
+}
